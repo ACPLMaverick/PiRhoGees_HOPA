2d957b5885a8b31b4300923734063491a14da5db
Jan Bełczącki <jan.belczacki@o2.pl>
2016-05-04 21:26:44 +0200

    [+] Added blank TutorialManager class [+] Added Letter class that inherits from PickableObject and put it on the Prototype Scene [*] After filling form, player will see Room 0 (Montage table) with letter and map

---
+++ HOPA/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : Singleton<TutorialManager> {
+
+    #region public
+
+
+
+    #endregion
+
+    #region functions
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    #endregion
+}
