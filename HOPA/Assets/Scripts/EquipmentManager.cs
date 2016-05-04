c4d9992f3d982c695fd57f0c8b81338d23bab73a
Marcin Wawrzonowski <MaverickCCCP@Gmail.com>
2016-05-04 22:30:12 +0200

    [*] Fixes in map and UI, they should now work properly.

--- HOPA/Assets/Scripts/EquipmentManager.cs
+++ HOPA/Assets/Scripts/EquipmentManager.cs
@@ -232,7 +232,7 @@ public class EquipmentManager : Singleton<EquipmentManager>
         _usableContainers = new UsableContainer[USABLE_MAX_ITEMS];
 
         Vector2 firstPos = PanelUsableList.position;
-        firstPos.y = PanelUsableList.position.y + PanelUsableList.rect.height * 0.39f;
+        firstPos.y = PanelUsableList.position.y + PanelUsableList.rect.height * 0.39f * PanelUsableList.GetComponent<Image>().canvas.scaleFactor;
         GameObject container = (GameObject)Instantiate(UsableListElementPrefab, firstPos, Quaternion.identity);
         float xDelta = (PanelUsableList.rect.height - (container.GetComponent<Image>()).rectTransform.rect.height) * 0.5f;
         float panelWidth = container.GetComponent<RectTransform>().rect.width;
@@ -263,8 +263,8 @@ public class EquipmentManager : Singleton<EquipmentManager>
         List<PickableObject> pickablesOnLevel = GameManager.Instance.CurrentRoom.PickableObjects;
 
         Vector2 firstPos = PanelPickableList.position;
-        firstPos.x -= PanelPickableList.rect.width * 0.35f;
-        firstPos.y += PanelPickableList.rect.height * 0.65f;
+        firstPos.x -= PanelPickableList.rect.width * 0.4f * PanelPickableList.GetComponent<Image>().canvas.scaleFactor;
+        firstPos.y += PanelPickableList.rect.height * 0.65f * PanelPickableList.GetComponent<Image>().canvas.scaleFactor;
         int i = 0;
         Vector2 nextPos = firstPos;
         foreach(PickableObject obj in pickablesOnLevel)
