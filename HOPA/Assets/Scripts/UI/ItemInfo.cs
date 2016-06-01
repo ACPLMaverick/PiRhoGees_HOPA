c9329ce45d7f17e85e4d81086a46ae8c959fad66
Marcin Wawrzonowski <MaverickCCCP@Gmail.com>
2016-06-02 00:13:39 +0200

    [*] Description shows only on objects with description given. [*] Equipment panel shows up when we switch it [*] Biggur pause button

--- HOPA/Assets/Scripts/UI/ItemInfo.cs
+++ HOPA/Assets/Scripts/UI/ItemInfo.cs
@@ -13,6 +13,14 @@ public class ItemInfo : MonoBehaviour
     private Button _backButton;
     private Vector2 _defSizeDelta;
 
+    private Vector3 _dPos;
+    private Vector3 _dScl;
+    private Vector3 _ndPos;
+    private Vector3 _ndScl;
+
+    private Vector3 _dTitPos;
+    private Vector3 _ndTitPos;
+
     #endregion
 
     #region functions
@@ -28,6 +36,16 @@ public class ItemInfo : MonoBehaviour
         _backButton = GetComponentInChildren<Button>();
         _backButton.onClick.AddListener(new UnityEngine.Events.UnityAction(OnBackButtonClick));
         _defSizeDelta = _img.rectTransform.sizeDelta;
+
+        _dPos = _img.GetComponent<RectTransform>().position;
+        _dScl = _img.GetComponent<RectTransform>().localScale;
+
+        _ndPos = GetComponent<RectTransform>().position;
+        _ndScl = _img.GetComponent<RectTransform>().localScale * 1.5f;
+
+        _dTitPos = _tit.rectTransform.position;
+        _ndTitPos = _dTitPos;
+        _ndTitPos.x = _tit.canvas.pixelRect.width * 0.5f;
     }
 
     // Use this for initialization
@@ -52,9 +70,36 @@ public class ItemInfo : MonoBehaviour
         nBounds.Normalize();
         nSizeDelta.x *= nBounds.x;
         nSizeDelta.y *= nBounds.y;
+
         _img.rectTransform.sizeDelta = nSizeDelta;
+        _img.rectTransform.position = _dPos;
+        _img.rectTransform.localScale = _dScl;
+
         _tit.text = title;
+        _tit.rectTransform.position = _dTitPos;
+
         _txt.text = text;
+    }
+
+    public void Show(Sprite sprite, string title)
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(Utility.FadeCoroutineUI(_grp, 0.0f, 1.0f, 0.65f, true));
+        _img.sprite = sprite;
+        Vector2 nSizeDelta = _defSizeDelta;
+        Vector2 nBounds = sprite.bounds.extents;
+        nBounds.Normalize();
+        nSizeDelta.x *= nBounds.x;
+        nSizeDelta.y *= nBounds.y;
+
+        _img.rectTransform.sizeDelta = nSizeDelta;
+        _img.rectTransform.position = _ndPos;
+        _img.rectTransform.localScale = _ndScl;
+
+        _tit.text = title;
+        _tit.rectTransform.position = _ndTitPos;
+
+        _txt.text = "";
     }
 
     public void Close()
